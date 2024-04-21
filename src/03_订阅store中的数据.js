a7beaf4f4cd1534e85828d93988dd294879e5b36
withLevi <zengyin.ssr@gmail.com>
2024-04-22 00:25:28 +0800

    Add store subscribe.

--- src/03_订阅store中的数据.js
+++ src/03_订阅store中的数据.js
@@ -1,19 +1,15 @@
 const store = require("./store");
 
-console.log(store.getState());
+const unsubscribe = store.subscribe( () => {
+    console.log("订阅数据变化: ", store.getState())
+})
 
 // 修改store中的数据
-const nameAction = { type: "change_name", name: "why" };
-store.dispatch(nameAction);
+store.dispatch({ type: "change_name", name: "why" });
+store.dispatch({ type: "change_name", name: "李磊" });
 
-console.log(store.getState());
-
-const nameAction2 = { type: "change_name", name: "李磊" };
-store.dispatch(nameAction2);
-console.log(store.getState());
+unsubscribe()
 
 // 修改counter
+store.dispatch({ type: "add_number", num: 10 })
 
-const counterAction = { type: "add_number", num: 10 };
-store.dispatch(counterAction)
-console.log(store.getState())
--- src/store/index-if判断.js
+++ src/store/index-if判断.js
@@ -13,15 +13,17 @@ const initialState = {
 // 参数二： 本次需要更新的action(dispatch传入的action)
 // 返回值:  它的返回值会作为store之后存储的state
 function reducer(state = initialState, action) {
-
-  switch(action.type) {
-    case "change_name":
-      return {...state, name: action.name}
-    case "add_number": 
-      return {...state, counter: state.counter + action.num}
-    default:
-      return state
+  // 有新数据进行更新的时候，那么返回一个新的store
+  if (action.type === "change_name") {
+    return { ...state, name: action.name };
+  } else if (action.type === "add_number") {
+    return { ...state, counter: state.counter + action.num };
   }
+
+  // 没有新数据更新, 那么返回之前的state
+
+  console.log("reducer: ", state, action);
+  return state;
 }
 
 // 创建的store
